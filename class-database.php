4ac049fb16014a9ccf414b4eaabd2bf66c03cd03
meshakeeb <info@spyropress.com>
2018-07-16 20:29:23 +0500

    Create class-database.php

---
+++ class-database.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The Query Builder
+ */
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Database class.
+ */
+class Database {
+
+	/**
+	 * Array of all databases objects.
+	 *
+	 * @var array
+	 */
+	protected static $instances = array();
+
+	/**
+	 * Retrieve a Database instance by table name.
+	 *
+	 * @param string $table_name A Database instance id.
+	 *
+	 * @return Database|bool False or Database object instance.
+	 */
+	public static function table( $table_name ) {
+
+		if ( empty( self::$instances ) || empty( self::$instances[ $table_name ] ) ) {
+			self::$instances[ $table_name ] = new Query_Builder( $table_name );
+		}
+
+		return self::$instances[ $table_name ];
+	}
+}
